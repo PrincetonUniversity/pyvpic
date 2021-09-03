b3d94d1801eb94be1db609dd2709178da48a808c
Jonathan Jara-Almonte <jjaraalm@pppl.gov>
2021-09-03 10:09:11 -0400

    Update toolbar to support matplotlib > 3.1.x

--- pyvpic/viewer/SliceToolbar.py
+++ pyvpic/viewer/SliceToolbar.py
@@ -1,6 +1,27 @@
 import os
+import enum
+from pkg_resources import packaging as pkg
 from PyQt5 import QtWidgets, QtCore, QtGui
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+import matplotlib
+
+
+MPL_VERSION = pkg.version.parse(matplotlib.__version__)
+
+
+class Mode(str, enum.Enum):
+    NONE = ""
+    PAN = "pan/zoom"
+    ZOOM = "zoom rect"
+    SLICE = "slice origin"
+
+    def __str__(self):
+        return self.value
+
+    @property
+    def _navigate_mode(self):
+        return self.name if self is not _Mode.NONE else None
+
 
 class VPICToolbar(NavigationToolbar):
 
@@ -46,17 +67,44 @@ class VPICToolbar(NavigationToolbar):
             trans = self.main_axes.transData.inverted()
             self.sliced.emit(*trans.transform((event.x, event.y)))
 
-    def _update_buttons_checked(self):
+    def _update_buttons_checked_mpl31x(self):
         """This is hackish, but works with current matplotlib.
-        Overrides def in matplotlib.backends.backend_qt5.NavigationToolbar2QT"""
+        Overrides def in matplotlib.backends.backend_qt5.NavigationToolbar2QT.
+        Works in matplotlib <= 3.1.x"""
+        super()._update_buttons_checked()
+        self._actions['slice'].setChecked(self._active == 'SLICE')
+
+    def _update_buttons_checked_mpl32x(self):
+        """This is hackish, but works with current matplotlib.
+        Overrides def in matplotlib.backends.backend_qt5.NavigationToolbar2QT.
+        Works in matplotlib == 3.2.x"""
         super()._update_buttons_checked()
 
         if 'slice' in self._actions:
             self._actions['slice'].setChecked(self._active == 'SLICE')
 
+    def _update_buttons_checked_mpl33x(self):
+        """This is hackish, but works with current matplotlib.
+        Overrides def in matplotlib.backends.backend_qt5.NavigationToolbar2QT.
+        Works in matplotlib >= 3.3.x"""
+        super()._update_buttons_checked()
+
+        if 'slice' in self._actions:
+            self._actions['slice'].setChecked(self.mode.name == 'SLICE')
+
+    def _update_buttons_checked(self):
+        """Update checked buttons based on matplotlib version."""
+        if MPL_VERSION < pkg.version.parse('3.2.0'):
+            self._update_buttons_checked_mpl31x()
+        elif MPL_VERSION < pkg.version.parse('3.3.0'):
+            self._update_buttons_checked_mpl32x()
+        else:
+            self._update_buttons_checked_mpl33x()
+
     def _set_cursor(self, event):
         """Also hackish, but allows us to change the cursor.
-        Overrides def in matplotlib.backend_bases.NavigationToolbar2"""
+        Overrides def in matplotlib.backend_bases.NavigationToolbar2.
+        Works with matplotlib <= 3.1.x"""
         active = self._active
         if active == 'SLICE':
             if self.main_axes.in_axes(event):
@@ -66,8 +114,42 @@ class VPICToolbar(NavigationToolbar):
         super()._set_cursor(event)
         self._active = active
 
-    def slice(self, checked):
-        """Handle enabling/disabling the slice button."""
+    def _update_cursor_mpl32x(self, event):
+        """Also hackish, but allows us to change the cursor.
+        Overrides def in matplotlib.backend_bases.NavigationToolbar2.
+        Works with matplotlib == 3.2.x"""
+        active = self._active
+        if active == 'SLICE':
+            if self.main_axes.in_axes(event):
+                self._active = 'ZOOM'
+            else:
+                self._active = False
+        super()._update_cursor(event)
+        self._active = active
+
+    def _update_cursor_mpl33x(self, event):
+        """Also hackish, but allows us to change the cursor.
+        Overrides def in matplotlib.backend_bases.NavigationToolbar2.
+        Works with matplotlib >= 3.3.x"""
+        mode = self.mode
+        if mode == Mode.SLICE:
+            if self.main_axes.in_axes(event):
+                self.mode = Mode.ZOOM
+            else:
+                self.mode = Mode.NONE
+        super()._update_cursor(event)
+        self.mode = mode
+
+    def _update_cursor(self, event):
+        """Update cursor based on matplotlib version."""
+        if MPL_VERSION < pkg.version.parse('3.3.0'):
+            self._update_cursor_mpl32x(event)
+        else:
+            self._update_cursor_mpl33x(event)
+
+    def slice_mpl32x(self, *args):
+        """Handle enabling/disabling the slice button. Works in
+        matplotlib < 3.3.x"""
         if self._active == 'SLICE':
             self._active = None
         else:
@@ -89,3 +171,33 @@ class VPICToolbar(NavigationToolbar):
         for axes in self.canvas.figure.get_axes():
             axes.set_navigate_mode(self._active)
         self._update_buttons_checked()
+
+    def slice_mpl33x(self, *args):
+        """Handle enabling/disabling the slice button. Works in
+        matplotlib >= 3.3.x"""
+        if self.mode == Mode.SLICE:
+            self.mode = Mode.NONE
+            self.canvas.widgetlock.release(self)
+        else:
+            self.mode = Mode.SLICE
+            self.canvas.widgetlock(self)
+
+        for axes in self.canvas.figure.get_axes():
+            axes.set_navigate_mode(self.mode)
+
+        self.set_message(self.mode)
+        self._update_buttons_checked()
+
+    def slice(self, *args):
+        """Handle enabling/disabling the slice button."""
+        if MPL_VERSION < pkg.version.parse('3.3.0'):
+            self.slice_mpl32x(*args)
+        else:
+            self.slice_mpl33x(*args)
+
+    def _zoom_pan_handler(self, event):
+        """Hackish, but handles mouse events in matplotlib >= 3.3.0."""
+        super()._zoom_pan_handler(event)
+        if self.mode == Mode.SLICE:
+            if event.name == 'button_press_event':
+                self.press_slice(event)
